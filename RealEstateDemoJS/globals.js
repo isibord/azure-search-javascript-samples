b3571644ec3449424c314a4766f3ea0dbee919b3
Daisy I <isibord@users.noreply.github.com>
2019-06-12 18:03:36 +0100

    customizing for ocp search
    customizing for ocp search

--- RealEstateDemoJS/globals.js
+++ RealEstateDemoJS/globals.js
@@ -1,8 +1,8 @@
 var availableTags = [];
-var azureSearchQueryApiKey = "595D275EDAA5A9B0268ECEE0AF22C159";	// this is a query key for demo purposes
-var baseSearchURL = "https://azs-playground.search.windows.net/indexes/realestate-us-sample";
+var azureSearchQueryApiKey = "CC7DA24CECCEDD6D9E10BCC3A918932F";	// this is a query key for demo purposes
+var baseSearchURL = "https://ocpsearch.search.windows.net/indexes/azureblob-index2";
 
 var facetFiltersString = [];
 var facetFiltersCollection = [];
 var currentPage = 1;
-var documentsToRetrieve = 15;	// This is the maximum documents to retrieve / page
+var documentsToRetrieve = 5;	// This is the maximum documents to retrieve / page
--- RealEstateDemoJS/search.js
+++ RealEstateDemoJS/search.js
@@ -4,51 +4,16 @@ function fullSearch(q)
 	if (q == "")
 		q = "*";
 	execSearch(q);
-	execMultiSelectFacetQuery(q, "city", "cityDiv", "string");
-	execMultiSelectFacetQuery(q, "tags", "tagsDiv", "collection");
 }
 
 function execSearch(q)
 {
 	// Execute a search
-	var searchAPI = baseSearchURL + "/docs?api-version=2017-11-11&$top=" + documentsToRetrieve + "&$skip=" + documentsToRetrieve * (currentPage - 1) + "&$count=true&queryType=full&search=" + q;
+	var searchAPI = baseSearchURL + "/docs?api-version=2019-05-06&$top=" + documentsToRetrieve + "&$skip=" + documentsToRetrieve * (currentPage - 1) + "&$count=true&queryType=full&search=" + q;
 	
-	// Build up the facet filters
 	// Create filter based on string fields
 	var filterQuery = '';
 	$( "#summaryContainer" ).html('');
-	for (var facet in facetFiltersString)
-	{
-		if (facetFiltersString[facet].length > 0)
-		{
-			var subFilterQuery = "(search.in(" + facet + ", '" + facetFiltersString[facet].join() + "', ','))";
-			if (filterQuery == '')
-			{
-				filterQuery = subFilterQuery;
-			}
-			else
-			{
-				filterQuery += " and " + subFilterQuery;
-			}
-		}
-	}
-
-	// Create filter based on collection fields
-	for (var facet in facetFiltersCollection)
-	{
-		if (facetFiltersCollection[facet].length > 0)
-		{
-			var subFilterQuery = "(" + facet + "/any(t: (search.in(t, '" + facetFiltersCollection[facet].join() + "', ','))))";
-			if (filterQuery == '')
-			{
-				filterQuery = subFilterQuery;
-			}
-			else
-			{
-				filterQuery += " and " + subFilterQuery;
-			}
-		}
-	}
 	
 	if (filterQuery != '')
 		searchAPI += "&$filter=" + filterQuery;
@@ -72,20 +37,10 @@ function execSearch(q)
 				for (var item in data["value"])
 				{
 				
-					htmlString += "<div class='card m-1'><div class='row'><div class='col-md-2 p-3'><div class='card-block px-3'>";
-					htmlString += "<img src='" + data.value[item].thumbnail + "'>";
+					htmlString += "<div class='card m-1'><div class='row'>";
 					
-					htmlString += "</div></div><div class='col-md-10 p-3'><div class='card-block px-3'>";
-					
-					htmlString += "<h4 class='card-title'>" + data.value[item].street + ", " + data.value[item].city + "</h4>";
-					htmlString += "<p class='card-text'>" + data.value[item].summary + "<br>";
-					
-					// Add the tags collection
-					for (var i = 0; i < data.value[item].tags.length; i++)
-						htmlString += "<span class='badge badge-info'>" + data.value[item].tags[i] + "</span>&nbsp;&nbsp;";
-					
-					htmlString += "</p>";
-					htmlString += "</div></div></div></div>";
+					htmlString += data.value[item].content.substring(0,150);;
+					htmlString += "</div></div>";
 				}
 			}
 			
